9c058f7a758af55d45651ef02d430a7c63aca9ba
Kyle Scheuing <kyle@kylescheuing.com>
2021-03-22 18:17:43 -0400

    Fix scroll bar hiding when resizing the window too small

--- widgets/scrollabletreeview.py
+++ widgets/scrollabletreeview.py
@@ -9,11 +9,17 @@ class ScrollableTreeview(tkinter.Frame):
         columns,
     ) -> None:
         super().__init__(master=master)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1)
+        self.rowconfigure(0, weight=1)
+
         self.tree_view = ttk.Treeview(self, columns=columns, show="headings")
-        self.tree_view.pack(side="left", fill="both", expand=True)
+        self.tree_view.grid(
+            row=0, column=0, sticky=tkinter.N + tkinter.S + tkinter.E + tkinter.W
+        )
 
         self.scroll_bar = ttk.Scrollbar(
             self, orient="vertical", command=self.tree_view.yview
         )
-        self.scroll_bar.pack(side="right", fill="y")
+        self.scroll_bar.grid(row=0, column=1, sticky=tkinter.N + tkinter.S)
         self.tree_view.configure(yscrollcommand=self.scroll_bar.set)
