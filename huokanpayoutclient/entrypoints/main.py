8f95e9c222849d1e7396e4f15fe0611785a7830e
Kyle Scheuing <kyle@kylescheuing.com>
2021-03-27 17:10:29 -0400

    Add error message boxes

--- huokanpayoutclient/entrypoints/main.py
+++ huokanpayoutclient/entrypoints/main.py
@@ -1,4 +1,6 @@
 import tkinter
+from tkinter import messagebox
+import traceback
 import locale
 from huokanpayoutclient.util.resources import get_resource_path
 from huokanpayoutclient.widgets.huokanwindow import HuokanWindow
@@ -10,8 +12,16 @@ def main():
     root = tkinter.Tk()
     root.minsize(300, 200)
     root.iconbitmap(get_resource_path("logo.ico"))
+
     window = HuokanWindow(master=root)
     window.update()
+
+    def alert_error(*args):
+        err = traceback.format_exception(*args)
+        messagebox.showerror(window.winfo_toplevel().title(), err)
+
+    root.report_callback_exception = alert_error
+
     root.mainloop()
 
 
